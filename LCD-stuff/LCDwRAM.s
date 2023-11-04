47e8afcfa62b30c3b4c1e25878e6a1f9243752e3
Sahil Mujadedi <sahilm@pop-os.localdomain>
2023-11-04 01:58:23 -0600

    busy flag checking, now works at 1MHz

--- LCD-stuff/LCDwRAM.s
+++ LCD-stuff/LCDwRAM.s
@@ -3,15 +3,17 @@ LOWERMASK=$0F
 
 PORTA=$6001
 DDRA=$6003
-; first 4 bits represent data
+; first 4 bits represent data, MSB to LSB
 RS=%00000010
 RW=%00000100
 E= %00001000
 
+
+
   .org $8000
 
 reset:  
-  LDA #%11111111 ; set all pins on port B to output
+  LDA #%11111111 ; set all pins on port A to output
   STA DDRA
   
   LDA #%00100000 ; set 4 bit mode
@@ -63,9 +65,38 @@ print_char:
   ; JSR lcd_instruction
   ; RTS
 lcd_instruction:
+  PHA
+  JSR check_busy
+  PLA
   STA PORTA      ; instruction cycle
   ORA #E         ; pull E bit high
   STA PORTA
   EOR #E         ; pull E bit low
   STA PORTA
+  RTS
+check_busy:      ; screws with accumulator, make sure to push it onto stack before running subroutine
+
+  LDA #(RS | RW | E)  ; set all data pins to input
+  STA DDRA
+
+  LDA #RW             ; enable read mode
+  STA PORTA
+  ORA #E
+  STA PORTA
+
+  LDA PORTA           ; read busy flag
+  PHA                 ; push busy flag onto stack
+  EOR #E
+  STA PORTA
+  ORA #E
+  STA PORTA
+  LDA PORTA           ; read other data, unnecessary but required read
+  PLA
+  AND #%10000000      ; if busy flag = 1, then loop check_busy
+  BNE check_busy
+
+  LDA #$ff            ; set pins back to outputs
+  STA DDRA
+
+
   RTS
