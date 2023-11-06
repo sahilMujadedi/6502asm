915e57c8f6b6eba87c5bd2d44ddaeaab71dc5e13
Sahil Mujadedi <sahilm@pop-os.localdomain>
2023-11-06 00:44:57 -0700

    shortened length of code, fully 8-bit instructions now.

--- LCD-stuff/LCDwRAM.s
+++ LCD-stuff/LCDwRAM.s
@@ -1,7 +1,9 @@
 UPPERMASK=$F0
 LOWERMASK=$0F
 
+PORTB=$6000
 PORTA=$6001
+DDRB=$6002
 DDRA=$6003
 ; first 4 bits represent data, MSB to LSB
 RS=%00000010
@@ -17,27 +19,27 @@ reset:
   STA DDRA
   
   LDA #%00100000 ; set 4 bit mode
-  JSR lcd_instruction
-  JSR lcd_instruction
+  JSR lcd_send
+  JSR check_busy
+  JSR lcd_send
   
   LDA #%10000000 ; set 2 line mode, 5x8 font
+  JSR lcd_send
+  JSR check_busy
+  ;; end of 4 bit set up. control from here is the exact same as 8 bit mode.
+  
+  LDA #%00001111 ; display on/off control
   JSR lcd_instruction
   
-  LDA #%00000000 ; display on/off control
-  JSR lcd_instruction
-  LDA #%11110000
+  LDA #%00000110 ; entry mode set
   JSR lcd_instruction
   
-  LDA #%00000000 ; entry mode set
+  LDA #%00000001 ; clear display
   JSR lcd_instruction
-  LDA #%01100000
-  JSR lcd_instruction
+
+
   
-  LDA #%00000000 ; clear display
-  JSR lcd_instruction
-  LDA #%00010000
-  JSR lcd_instruction
-  
+ 
   LDX #0
 print:
   LDA message,x   ; use x as index to continually print out chars in "message"
@@ -49,25 +51,42 @@ print:
 loop:
   JMP loop
   
-message: .asciiz "Sahil is cool" ; creates a null-terminated string in memory
+message: .asciiz "I'm alive!" ; creates a null-terminated string in memory
 print_char:
   PHA                  ; push a onto stack to preserve the char
-  AND #UPPERMASK       ; manipulate it for the high nibble
-  ORA #RS
-  JSR lcd_instruction
-  PLA                  ; pull a back to use the lower nibble
-  ROL
-  ROL
-  ROL
-  ROL
-  AND #UPPERMASK
-  ORA #RS
-  ; JSR lcd_instruction
-  ; RTS
-lcd_instruction:
   PHA
   JSR check_busy
   PLA
+
+  AND #UPPERMASK       ; manipulate it for the high nibble
+  ORA #RS
+
+  JSR lcd_send
+  PLA                  ; pull a back to use the lower nibble
+  ASL
+  ASL
+  ASL
+  ASL
+  ORA #RS
+  JSR lcd_send
+  RTS
+lcd_instruction:
+  PHA
+  PHA
+  JSR check_busy
+  PLA
+
+  AND #UPPERMASK
+  
+  JSR lcd_send
+  PLA
+  ASL
+  ASL
+  ASL
+  ASL
+  ; JSR lcd_send
+  ; RTS
+lcd_send:
   STA PORTA      ; instruction cycle
   ORA #E         ; pull E bit high
   STA PORTA
@@ -97,6 +116,5 @@ check_busy:      ; screws with accumulator, make sure to push it onto stack befo
 
   LDA #$ff            ; set pins back to outputs
   STA DDRA
-
 
   RTS
